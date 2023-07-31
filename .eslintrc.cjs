19eb689203631e76635f109107cb914bb87f4081
aniamarkh <aniamarkh@gmail.com>
2023-07-31 22:23:43 +0300

    init: config nuxt, eslint, prettier, tailwind

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+};
