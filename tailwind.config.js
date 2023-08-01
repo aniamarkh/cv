cd356fb288ad98725a5d30af8394e698a121b4fb
aniamarkh <aniamarkh@gmail.com>
2023-08-02 00:45:27 +0300

    feat: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,38 @@
 module.exports = {
   content: ['./**/*.vue'],
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      primary: ['Roboto Mono'],
+    },
     screens: {
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
     },
+    colors: {
+      current: 'currentColor',
+      grey: '#F1F1F1',
+      white: '#FEFEFE',
+      black: '#121212',
+      accent: '#EC9FD6',
+    },
     extend: {
-      colors: {
-        primary: '#0A66C2',
-        lighter: '#378fe9',
-        darker: '#004182',
+      width: {
+        192: '48rem',
+        lg: '64rem',
+      },
+      keyframes: {
+        bounceIn: {
+          '0%': { transform: 'scale(0.3)' },
+          '50%': { transform: 'scale(1.05)' },
+          '90%': { transform: 'scale(0.9)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        bounceIn: 'bounceIn 0.5s ease-in-out',
       },
     },
   },
