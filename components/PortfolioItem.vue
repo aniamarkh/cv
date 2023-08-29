24bc57ee94bcf05ddb4e35e7d0662e28ca47e49e
aniamarkh <aniamarkh@gmail.com>
2023-08-29 16:04:15 +0300

    fix: portfolio section layout

--- components/PortfolioItem.vue
+++ components/PortfolioItem.vue
@@ -2,6 +2,7 @@
 const props = defineProps<{
   title: string;
   desc: string;
+  built: string;
   repo: string;
   deploy: string;
   screenshot: string;
@@ -14,25 +15,30 @@ const openInNewTab = () => {
 
 <template>
   <div
-    class="cursor-pointer group justify-self flex flex-col gap-1 max-w-xl p-5 border-solid border border-black hover:scale-105 hover:bg-accent hover:border-accent dark:border-lightgrey dark:hover:text-black dark:hover:border-accent transition-all duration-300"
+    class="cursor-pointer group justify-self items-center flex flex-row sm:flex-col gap-4 w-full p-5 border-solid border border-black hover:scale-105 hover:bg-accent hover:border-accent dark:border-lightgrey dark:hover:text-black dark:hover:border-accent transition-all duration-300"
     @click="openInNewTab"
   >
     <img class="screenshot" :src="screenshot" :alt="`${title} screenshot`" />
-    <h2 class="mt-2 text-lg font-bold">{{ title }}</h2>
-    <p>{{ desc }}</p>
-    <div class="flex flex-row gap-5 mt-3">
-      <a :href="repo" target="_blank" class="text-link whitespace-nowrap" @click.stop>
-        <svg class="contacts-icon" viewBox="0 0 100 100" aria-hidden="true">
-          <path
-            d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-          />
-        </svg>
-        View Source
-      </a>
-      <a :href="deploy" target="_blank" class="text-link whitespace-nowrap" @click.stop>
-        <span class="material-symbols-outlined"> play_arrow </span>
-        Live Demo
-      </a>
+    <div class="flex flex-col h-full justify-between">
+      <h2 class="mt-2 text-lg font-bold">{{ title }}</h2>
+      <div class="flex flex-col gap-2">
+        <p>{{ desc }}</p>
+        <p>{{ 'Built with ' + built }}</p>
+      </div>
+      <div class="flex flex-row gap-5 mt-3">
+        <a :href="repo" target="_blank" class="text-link whitespace-nowrap" @click.stop>
+          <svg class="contacts-icon" viewBox="0 0 100 100" aria-hidden="true">
+            <path
+              d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+            />
+          </svg>
+          View Source
+        </a>
+        <a :href="deploy" target="_blank" class="text-link whitespace-nowrap" @click.stop>
+          <span class="material-symbols-outlined"> play_arrow </span>
+          Live Demo
+        </a>
+      </div>
     </div>
   </div>
 </template>
--- components/PortfolioSection.vue
+++ components/PortfolioSection.vue
@@ -2,42 +2,56 @@
 const portfolioItems = [
   {
     title: 'Positivus Landing',
-    desc: 'Vue 3, SCSS, pixel-perfect, accessible and responsive',
+    desc: 'An example of an adaptive and accessible pixel-perfect landing page, demonstrating my attention to detail and knowledge of  HTML and modern CSS layout.',
+    built: 'Vue 3, Vite and SCSS',
     repo: 'https://github.com/aniamarkh/positivus-landing',
     deploy: 'https://positivus-landing-aniamarkh.vercel.app/',
     screenshot: 'positivus.png',
   },
   {
     title: 'Weather App',
-    desc: 'Vue 3 & TS, SCSS, Vuex, ECharts, REST API, accessible, mobile-first',
+    desc: 'An SPA that demonstrates my skills in working with REST API and asynchronous operations, developed with a mobile-first approach, accessible.',
+    built: 'Vue 3 & TS, Vite, SCSS, Vuex and ECharts',
     repo: 'https://github.com/aniamarkh/weather-forecast',
     deploy: 'https://weather-forecast-aniamarkh.vercel.app/',
     screenshot: 'weather.png',
   },
   {
     title: 'Kanban Board',
-    desc: 'Vue 3 & TS, SCSS, Pinia, drag&drop, Vitest & Vue Testing Utils',
+    desc: 'Demonstrates my skills in working with state managers, as well as handling forms and their validation.',
+    built: 'Vue 3 & TS, SCSS, Pinia, drag&drop, Vitest & Vue Testing Utils',
     repo: 'https://github.com/aniamarkh/kanban-board',
     deploy: 'https://kanban-aniamarkh.vercel.app/',
     screenshot: 'kanban.png',
   },
   {
+    title: 'Movie App',
+    desc: `Project created during the RS School React course: demonstrates basic knowledge of React and its ecosystem. Allows users to search for movies using the themoviedb API. It also features a validated form page.`,
+    built: 'React, Vite, Vitest, React Redux, React Router and React Hook Form',
+    repo: 'https://github.com/aniamarkh/rsschool-react',
+    deploy: 'https://rsschool-react-coral.vercel.app/',
+    screenshot: 'movie.png',
+  },
+  {
     title: "'Other bookmarks' Manager",
-    desc: 'Chrome Extension, Vue 3 & TS, drag&drop, custom theme',
+    desc: 'Drag-and-drop manager for the Other Bookmarks folder in Chrome New Tab with a custom theme and full data synchronization through the Chrome API.',
+    built: 'Chrome API, Vue 3 & TS, Vue.Draggable and Vueform',
     repo: 'https://github.com/aniamarkh/bookmarks',
     deploy: 'https://chrome.google.com/webstore/detail/kartotek/hcinbgecljcibhpfpjibkodifeaanjep',
     screenshot: 'bookmarks.png',
   },
   {
     title: 'Kettle Control Station',
-    desc: "Vue 3 & TS, Node.js, SCSS, Websocket, password is 'porol' ;)",
+    desc: `An app that allows my friend to remotely control his kettle through a web interface after authorization, it connects to the kettle's retrofitted microcontroller via WebSocket. A Node.js server has been implemented for demonstration purposes.`,
+    built: 'Vue 3 & TS, Node.js, SCSS, Websocket',
     repo: 'https://github.com/aniamarkh/kettle-station',
     deploy: 'https://kettle-station.vercel.app/',
     screenshot: 'kettle.png',
   },
   {
     title: 'RS Lang',
-    desc: 'RS School course final team-project, built with TS',
+    desc: 'RS School course final team-project: an app for learning foreign words with an electronic textbook, mini-games and a statistics page to track progress.',
+    built: 'TS and Chart.js',
     repo: 'https://github.com/aniamarkh/rslang',
     deploy: 'https://rslang-aniamarkh.vercel.app/',
     screenshot: 'lang.png',
@@ -51,12 +65,13 @@ const portfolioItems = [
       <span class="material-symbols-outlined text-xl" aria-hidden="true"> construction </span>Recent
       projects
     </h1>
-    <div class="grid grid-cols-2 md:grid-cols-1 gap-6">
+    <div class="grid grid-cols-1 gap-6">
       <PortfolioItem
         v-for="(item, index) of portfolioItems"
         :key="index"
         :title="item.title"
         :desc="item.desc"
+        :built="item.built"
         :repo="item.repo"
         :deploy="item.deploy"
         :screenshot="`../assets/${item.screenshot}`"
